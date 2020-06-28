7d3d0e293003093f218aca42d7aa8bc20d303f08
Pablo Lucero <plucero87@gmail.com>
2020-06-28 18:43:28 -0300

    complete Question 3

---
+++ src/main/java/com/trustar/interview/question3/Question3.java
@@ -0,0 +1,55 @@
+package com.trustar.interview.question3;
+
+import com.trustar.interview.exception.QuestionException;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Question3 {
+
+    private Question3() {
+    }
+
+    public static List<String> findPatterns(List<Pattern> patterns, String textInput) {
+        return findPatterns(patterns, textInput, null);
+    }
+
+    public static List<String> findPatterns(List<Pattern> patterns, String textInput, Pattern patternToIgnore) {
+
+        if (patterns == null || patterns.isEmpty()) {
+            throw new QuestionException("Patterns is null or empty");
+        }
+
+        textInput = removeMatches(textInput, patternToIgnore);
+
+        List<String> result = new ArrayList<>();
+
+        for (Pattern pattern : patterns) {
+            textInput = selectMatchesAddThemToResultAndReturnTextInputWithoutThoseMatches(pattern, textInput, result);
+        }
+        return result;
+    }
+
+    private static String selectMatchesAddThemToResultAndReturnTextInputWithoutThoseMatches(Pattern pattern,
+                                                                                            String textInput,
+                                                                                            List<String> result) {
+        final Matcher regexMatcher = pattern.matcher(textInput);
+        while (regexMatcher.find()) {
+            if (regexMatcher.group().length() > 0) {
+                result.add(regexMatcher.group());
+                textInput = textInput.replaceFirst(pattern.pattern(), "");
+            }
+        }
+        return textInput;
+    }
+
+    private static String removeMatches(String textInput, Pattern patternToIgnore) {
+        if (patternToIgnore != null) {
+            return textInput.replaceAll(patternToIgnore.pattern(), "");
+        }
+        return textInput;
+    }
+
+}
---
+++ src/test/java/com/trustar/interview/question3/Question3Test.java
@@ -0,0 +1,57 @@
+package com.trustar.interview.question3;
+
+import com.google.common.collect.ImmutableList;
+import com.trustar.interview.exception.QuestionException;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.regex.Pattern;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class Question3Test {
+
+    @Test
+    public void exceptionIsThrownIfThePattersListIsNull() {
+        assertThrows(QuestionException.class, () -> Question3.findPatterns(null, "abc"));
+    }
+
+    @Test
+    public void exceptionIsThrownIfThePattersListIsEmpty() {
+        assertThrows(QuestionException.class, () -> Question3.findPatterns(ImmutableList.of(), "abc"));
+    }
+
+    @Test
+    public void emptyTextInputReturnsAnEmptyList() {
+        List<Pattern> patterns = ImmutableList.of(Pattern.compile("."));
+
+        final List<String> actual = Question3.findPatterns(patterns, "");
+        final List<String> expected = ImmutableList.of();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void onlyOnePatternMatch() {
+        List<Pattern> patterns = ImmutableList.of(Pattern.compile("(the)"));
+
+        final List<String> actual = Question3.findPatterns(patterns, "The fox jumped over the fence");
+        final List<String> expected = ImmutableList.of("the");
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void multiplePatternsMatched() {
+        final List<Pattern> patterns = ImmutableList.of(
+                Pattern.compile("(the)"),
+                Pattern.compile("(fox|fence)"),
+                Pattern.compile("(abc)")
+        );
+
+        final Pattern patternToIgnore = Pattern.compile("f.x");
+        final List<String> actual = Question3.findPatterns(patterns, "The fox jumped over the fence", patternToIgnore);
+        final List<String> expected = ImmutableList.of("the", "fence");
+        assertEquals(expected, actual);
+    }
+
+}
